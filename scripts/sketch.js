e5b3693cffb14d1d5894c7f0ff0c9cc65dae5c6d
Mark R <mwritchie2@gmail.com>
2016-04-07 22:19:28 -0500

    like half there

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,7 +1,44 @@
 $(document).ready(function() {
-  $('h3.banner').
-  for (this.i = 0; i < 16; i++) {
-    for (;;) {};
+  $('#topWrap').append($('<div id="grid"></div>'));
+  /*
+  var popGrid
+  var addRow = function(num) {
+    do {
 
+    }
+  };*/
+  function addScrib() {
+    $('#grid').append($('<div id="scrib"></div>'));
   };
+
+  function addRow(num) {
+    do {
+      addScrib();
+      num--;
+    } while (num > 0);
+    $('#grid').append($('<br>'));
+  };
+
+  function popGrid(sqSize) {
+    $('#scrib').height(function() {
+      return 900 / sqSize;
+    });
+    $('#scrib').width(function() {
+      return (900 / this.sqSize);
+    });
+    this.i = sqSize;
+    do {
+      addRow(sqSize);
+      i--;
+    } while (i > 0);
+  };
+
+  var size = 16;
+
+  $('#setSize').click(function() {
+    size = $('#square').val();
+    $('#grid').empty();
+    popGrid(size);
+  });
+  popGrid(size);
 });
